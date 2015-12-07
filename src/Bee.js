388d83eccebe2579186489b6145a78ced77f4e8e
HarrisonA <radiov@gmail.com>
2015-12-07 15:33:23 -0800

    Completed requirements for taxonomy

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,18 @@
 var Bee = function() {
+  //this = Object.create(Bee.prototype);
+  
+  //inherit all of Grub's properties
+  Grub.call(this);  
+
+  
+  this.age = 5;  //update the age property
+  this.color = "yellow";  //update the color property
+  this.job = "keep on growing"; //create a job property because Grub doesn't assign a job property
+
 };
+
+Bee.prototype = Object.create(Grub.prototype);  //creates a blank object that will fall back to Grub
+Bee.prototype.constructor = Bee;  //Because the above line set Bee's prototype to a new empty object, we need to reassign the constructor property to Bee other wise it will fall thorugh to Grub
+
+
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  //this = Object.create(HoneyMakerBee.prototype);
+
+  Bee.call(this);  //inherit all the Bee properties
+
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot =0;
+  
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot--;
+};
